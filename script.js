a204babee2275cfaee2dd3248b679cbba42ed37b
remsokawaii1 <remsokawaii1@gmail.com>
2021-10-07 16:10:39 +0700

    Login complete

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ function hideMenu() {
     navLink.style.right = "-200px";
 }
 
-function logIn(){
+
+function logIn() {
     alert("Tên đăng nhập hoặc mật khẩu không tồn tại!")
 }
+
+
+(function() {
+    var forms = document.querySelectorAll(".needs-validation");
+    Array.prototype.slice.call(forms).forEach((form) => {
+        form.addEventListener("submit", (event) => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+        }, false)
+    })
+})()
