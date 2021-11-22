b600831bf99b775da7bfa331da4423a6a371c43b
thinhtruong2112 <truonggiathinh2112@gmail.com>
2021-11-22 21:16:00 +0700

    22/11/2021

--- mvc/views/addMember.php
+++ mvc/views/addMember.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Multilevel Company</title>
+    <title>Mona Company</title>
     <link rel="stylesheet" href="./public/css/member.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -114,6 +114,6 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js "></script>
-    <script src="./public/js/member.js"></script>
+    <script src="./public/js/script.js"></script>
 </body>
 </html>
