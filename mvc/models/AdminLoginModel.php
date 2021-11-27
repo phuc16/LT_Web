8d0d106b0eab1bc6d55150e859efdb10dd8aecd3
thinhtruong2112 <truonggiathinh2112@gmail.com>
2021-11-27 10:53:23 +0700

    27/11/2021

--- mvc/models/AdminLoginModel.php
+++ mvc/models/AdminLoginModel.php
@@ -3,7 +3,8 @@
 
         public function verifyLogin($username, $password) {
             $query = "SELECT * FROM admins WHERE username = '$username' AND password = '$password'";
-            if (mysqli_query($this->con, $query)) {
+            $result = mysqli_query($this->con, $query);
+            if (mysqli_num_rows($result)) {
                 return true;
             }
             return false;
