ff40a00111ac0873dfa2dd44b6dce15ec99d3084
kogleo <hellresiak@gmail.com>
2021-11-28 19:29:50 +0700

    add alert for add member

--- mvc/models/AddMemberModel.php
+++ mvc/models/AddMemberModel.php
@@ -4,7 +4,21 @@
         public function addMember($username, $name, $mail, $phone, $avatar, $dob, $gender, $password) {
             $query = "INSERT INTO members(username, name, email, phone, avatar, dob, gender, password) VALUES ('$username', '$name', '$mail', '$phone', '$avatar', 
                     '$dob', '$gender', '$password');";
-            mysqli_query($this->con, $query);
+             if (mysqli_query($this->con, $query)) {
+                echo "<script type='text/javascript'>alert('Thêm thành công');</script>";
+                return true;
+            }
+            else
+            {
+                if(mysqli_errno($this->con) == 1062)
+                {
+                     echo "<script type='text/javascript'>alert('username đã tồn tại');</script>";
+                } 
+                else{
+                    echo "<script type='text/javascript'>alert('Thêm thất bại');</script>";
+                }
+                return false;
+            }
         }
     }
 ?>
